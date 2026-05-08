cf3a25edbf85c36b4b55de1c15ab122e7785640d
Sikandar Farooq Saani <129685362+SikandarFarooqSaani@users.noreply.github.com>
2026-05-08 23:30:48 +0500

    Add files via upload

---
+++ huggingface_model.py
@@ -0,0 +1,15 @@
+from langchain_huggingface import ChatHuggingFace, HuggingFaceEndpoint
+from dotenv import load_dotenv
+
+load_dotenv()
+llm = HuggingFaceEndpoint(
+    repo_id="deepseek-ai/DeepSeek-V4-Pro",
+    task="text-generation"
+)
+model = ChatHuggingFace(llm = llm)
+
+result = model.invoke("Capital of Pakistan ?")
+
+print(result.content)
+
+#py 2.ChatModels/huggingface.py
